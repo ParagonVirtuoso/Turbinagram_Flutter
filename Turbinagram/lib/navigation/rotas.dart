7c0f21f46f01622058c1166b243f47632d37a12e
Denys Fernandes <38758938+denysfer@users.noreply.github.com>
2022-03-31 22:48:47 -0300

    Update rotas.dart

--- Turbinagram/lib/navigation/rotas.dart
+++ Turbinagram/lib/navigation/rotas.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:turbinagram/screens/cadastro.dart';
 import 'package:turbinagram/screens/entrar.dart';
 import 'package:turbinagram/screens/home.dart';
+import 'package:turbinagram/screens/turbinagram_inicial.dart';
 
 class Rotas {
   static Map<String, Widget Function(BuildContext)> routes = {
-    '/': (context) => Entrar(),
-    '/home': (context) => Home(),
-    '/cadastro': (context) => Cadastro()
+    '/': (context) => const Entrar(),
+    '/home': (context) => const Home(),
+    '/cadastro': (context) => const Cadastro(),
+    '/turbinagram_inicial': (context) => const TurbinagramInicial()
   };
 }
