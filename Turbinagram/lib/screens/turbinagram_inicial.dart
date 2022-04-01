fcff2725789bf2bc61c3b0cb37a793c0ee45d236
Denys Fernandes <38758938+denysfer@users.noreply.github.com>
2022-03-31 22:44:45 -0300

    Create turbinagram_inicial.dart

---
+++ Turbinagram/lib/screens/turbinagram_inicial.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TurbinagramInicial extends StatefulWidget {
+  const TurbinagramInicial({Key? key}) : super(key: key);
+
+  @override
+  State<TurbinagramInicial> createState() => _TurbinagramInicialState();
+}
+
+class _TurbinagramInicialState extends State<TurbinagramInicial> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
