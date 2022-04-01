70cd9f7f85de65ba9b383a3516c62461c57cb992
Denys Fernandes <38758938+denysfer@users.noreply.github.com>
2022-04-01 00:29:57 -0300

    Update turbinagram_inicial.dart

--- Turbinagram/lib/screens/turbinagram_inicial.dart
+++ Turbinagram/lib/screens/turbinagram_inicial.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:turbinagram/utils/strings.dart';
 
 class TurbinagramInicial extends StatefulWidget {
   const TurbinagramInicial({Key? key}) : super(key: key);
@@ -10,6 +12,16 @@ class TurbinagramInicial extends StatefulWidget {
 class _TurbinagramInicialState extends State<TurbinagramInicial> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Center(
+          child: Container(
+            width: 100.w,
+            padding: EdgeInsets.only(bottom: 1300.h),
+            child: Image.asset("assets/logo_final-pequeno.png",color: Strings.kDarkGreyColor.withOpacity(0.5)),
+          ),
+        )
+      ],
+    );
   }
 }
