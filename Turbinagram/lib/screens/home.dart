e69d32ded763a85f9055820b06b664ba0a76d590
Denys Fernandes <38758938+denysfer@users.noreply.github.com>
2022-04-01 01:56:27 -0300

    Update home.dart

--- Turbinagram/lib/screens/home.dart
+++ Turbinagram/lib/screens/home.dart
@@ -12,7 +12,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  late List<String>? dados = [];
+  late List<String>? dados = [''];
 
   @override
   void initState() {
